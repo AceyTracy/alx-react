1f837885df8548c980f17b288b937c411a33bcf1
AceyTracy <werutracy@gmail.com>
2023-08-17 12:46:18 +0000

    tasks

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,5 +1,22 @@
-import { fromJS } from 'immutable';
+// Import the required module from Immutable.js library
+const { fromJS, Map } = require('immutable');
 
-export default function getImmutableObject(object) {
+// Define the function that converts the object to an immutable Map
+function getImmutableObject(object) {
+  // Use the fromJS function to convert the object to an immutable Map
   return fromJS(object);
 }
+
+// Example object
+const exampleObject = {
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132
+};
+
+// Convert the example object to an immutable Map using the function
+const immutableMap = getImmutableObject(exampleObject);
+
+// Print the resulting immutable Map
+console.log(immutableMap);
